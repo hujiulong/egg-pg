e1ccfc1694621ed41c355150e293ec0eed9e9b59
zemzheng <zemzheng@tencent.com>
2018-07-23 20:51:31 +0800

    新增 ts 支持

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+import { Client, ClientConfig } from 'pg';
+
+declare module 'egg' {
+
+  // extend app
+  interface Application {
+    pg?: Client,
+  }
+
+  // // extend agent
+  // interface Agent{
+  //   pg?: Client,
+  // }
+
+  // // extend context
+  // interface Context {
+  // }
+
+  // extend your config
+  interface EggAppConfig {
+    pg: {
+        client: ClientConfig
+        clients: {
+            [key: string]: ClientConfig,
+        },
+        app: Boolean,
+        agent: Boolean,
+        pool: Boolean,
+    },
+  }
+
+}
